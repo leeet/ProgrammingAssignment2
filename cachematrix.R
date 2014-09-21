9f79adeb467395f1d8e3eac3108f0e414b49838e
leeet <leeeng@gmail.com>
2014-09-21 18:50:17 +0800

    Assign 2 - 2nd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function takes in a matrix, create cache and return cached matrix 
+## To run:
+## > d <- matrix(1:4 nrow=2, ncol=2)
+## > a <- makeCacheMatrix(d)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		iflag  <- 0
+		set <- function(y)
+		{
+			x <<- y
+			iflag <<- 0
+		}
+		get <- function() x
+		getflag <- function() iflag
+		setflag <- function() iflag <<- 1
+		list(set = set, get = get, 
+			getflag = getflag, 
+			setflag = setflag)
+		
 }
 
 
-## Write a short comment describing this function
+## Function to check if invert cache exist. if yes message displayed and
+## cached inverse retrived. If none, inverse matrix and cache
+## To run:
+## > cacheSolve(a)
 
 cacheSolve <- function(x, ...) {
+	   data <- x$get()		
+	   lflag <- x$getflag()
+	   if(lflag == 1)
+	   {
+		message("Retrieve inverse matrix from cache.")
+		return(data)
+	   }
+	   idata <- solve(data)
+	   x$set(idata)
+	   x$setflag()
+	   
         ## Return a matrix that is the inverse of 'x'
+	  return(idata)
+	  
 }
